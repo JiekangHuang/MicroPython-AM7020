17c53339d98a873e03be01235400053399e209ec
Zack Huang <zack@atticedu.com>
2021-03-16 14:13:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# AM7020 MicroPython 範例程式碼
+ [AM7020](https://atticedu.com/index.php/am7020.html) (SIMCOM SIM7020E) 窄頻物聯網(NBIoT)模組 MicroPython 範例程式碼
+ 
+  ![AM7020](images/am7020_front.jpg)
+
+ Installation
+ ------------
+
+ To install am7020:
+
+ ```python
+ import upip
+ upip.install('micropython-am7020')
+ ```
+ or
+ ```console
+ $ ampy -p COMX put am7020
+ ```
+ ## MQTT 教學說明
+ Declare nb and mqtt client instance using:
+ ```Python
+ nb = SIM7020NB(port="/dev/ttyS0", baudrate=115200, reset_pin=18)
+ mqtt = SIM7020MQTT(nb)
+ ```
+ Initialize nb and connect to NBIOT base station:
+ ```Python
+ while((not nb.init() or (not nb.nbiotConnect(apn, band)))):
+     print(".")
+ 
+ while(not nb.waitForNetwork()):
+     print(".")
+     sleep(5)
+ ```
+ Check MQTT connection status and connect to Broker:
+ ```Python
+ if(not mqtt.chkConnBroker()):
+     mqtt.connBroker(MQTT_BROKER, 1883, mqtt_id="MY_AM7020_TEST_MQTTID")
+ ```
+ Publish and subscribe:
+ ```Python
+ mqtt.publish(TEST_TOPIC, "Hello MQTT")
+ 
+ def callback1(msg):
+     print(TEST_TOPIC, ":", msg)
+ mqtt.subscribe(TEST_TOPIC, callback1)
+ ```
+ Listen for messages from the broker:
+ ```Python
+ mqtt.procSubs()
+ ```
+ ## HTTP 教學說明
+ Declare nb and http client instance using:
+ ```Python
+ nb = SIM7020NB(port="/dev/ttyS0", baudrate=115200, reset_pin=18)
+ http = SIM7020HTTP(nb, HTTP_SERVER)
+ ```
+ Initialize nb and connect to NBIOT base station:
+ ```Python
+ while((not nb.init() or (not nb.nbiotConnect(apn, band)))):
+     print(".")
+ 
+ while(not nb.waitForNetwork()):
+     print(".")
+     sleep(5)
+ ```
+ HTTP GET and POST:
+ ```Python
+ http.get(HTTP_GET_API)
+ state_code = http.responseStatusCode()
+ body = http.responseBody()
+ 
+ http.post(HTTP_POST_API, content_type="application/json", body="{\"value\": \"POST\"}")
+ state_code = http.responseStatusCode()
+ body = http.responseBody()
+ ```
---
+++ am7020/am7020_modem.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# am7020_modem.py
+# @Author : Zack Huang ()
+# @Link   : zack@atticedu.com
+# @Date   : 2020/11/5 下午1:40:47
+
+import serial
+from time import time, sleep
+from gpiozero import LED
+
+GSM_OK = "OK\r\n"
+GSM_ERROR = "ERROR\r\n"
+
+
+class AM7020Modem:
+    def __init__(self, port, baudrate, reset_pin, dump_at_cmd=False):
+        self._at = serial.Serial(port, baudrate, timeout=0.05)
+        self._reset_pin = LED(reset_pin)
+        self.dump_at_cmd = dump_at_cmd
+
+    def atWrite(self, cmd):
+        if(self.dump_at_cmd):
+            print(cmd, end="")
+        cmd = bytes(cmd, 'utf-8')
+        self._at.write(cmd)
+
+    def atRead(self, numChars=1):
+        try:
+            cmd = self._at.read(numChars).decode("utf-8")
+            if(self.dump_at_cmd):
+                print(cmd, end="")
+            return cmd
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print("decode error !")
+            return ""
+
+    def restart(self):
+        self._reset_pin.off()
+        sleep(0.5)
+        self._reset_pin.on()
+        sleep(5)
+
+    def testAT(self, timeout_s=10):
+        startTime = time()
+        while(time() - startTime < timeout_s):
+            self.sendAT()
+            if(self.waitResponse(0.2) == 1):
+                return True
+            sleep(0.1)
+        return False
+
+    def streamWrite(self, *args):
+        cmd = ""
+        for arg in args:
+            cmd += str(arg)
+        self.atWrite(cmd)
+
+    def sendAT(self, *args):
+        cmd = "AT"
+        for arg in args:
+            cmd += str(arg)
+        cmd += "\r\n"
+        self.atWrite(cmd)
+
+    def streamRead(self):
+        self.atRead()
+
+    def streamGetLength(self, numChars, timeout_s=1):
+        startTime = time()
+        data = ""
+        while(time() - startTime < timeout_s):
+            data += self.atRead(numChars)
+            if(data != "" and len(data) == numChars):
+                return data
+
+    def streamGetIntBefore(self, lastChar, timeout_s=1):
+        startTime = time()
+        data = ""
+        while(time() - startTime < timeout_s):
+            data += self.atRead()
+            if(data != "" and data.endswith(lastChar)):
+                return int(data[:-1])
+        return -9999
+
+    def streamGetStringBefore(self, lastChar, timeout_s=1):
+        startTime = time()
+        data = ""
+        while(time() - startTime < timeout_s):
+            data += self.atRead()
+            if(data != "" and data.endswith(lastChar)):
+                return data[:-1]
+        return ""
+
+    def streamSkipUntil(self, c, timeout_s=1):
+        startTime = time()
+        while(time() - startTime < timeout_s):
+            ch = self.atRead()
+            if(ch == c):
+                return True
+        return False
+
+    def waitResponse(self, timeout_s=1, r1=GSM_OK, r2=GSM_ERROR, r3=None, r4=None, r5=None):
+        index = 0
+        startTime = time()
+        data = ""
+        while(True):
+            data += self.atRead()
+            if(r1 and data.endswith(r1)):
+                index = 1
+                break
+            elif(r2 and data.endswith(r2)):
+                index = 2
+                break
+            elif(r3 and data.endswith(r3)):
+                index = 3
+                break
+            elif(r4 and data.endswith(r4)):
+                index = 4
+                break
+            elif(r5 and data.endswith(r5)):
+                index = 5
+                break
+            if(time()-startTime > timeout_s):
+                break
+        return index
